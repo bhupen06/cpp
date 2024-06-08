a047b3029b39e578a307b858e35c063b67c580ca
bhupen06 <bhupendraprajapat046@gmail.com>
2024-06-08 12:49:46 +0530

    first cpp program

---
+++ first.cpp
@@ -0,0 +1,14 @@
+//addition program
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter number 1st : ";
+    cin>>a;
+    cout<<"enter number 2nd : ";
+    cin>>b;
+    sum=a+b;
+    cout<<"sum : "<<sum;
+  return 0;
+}
